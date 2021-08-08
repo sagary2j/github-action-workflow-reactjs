d4f0524e6c4cb51fb33437795a903d5a58e8fc11
Sagar R <sagar.ravkhande@gmail.com>
2021-08-09 02:10:02 +0530

    updated tf code

---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+variable "ecr_values" {
+  type        = any
+  default     = {}
+  description = "AWS ECR configuration"
+}
+
+variable "ecs_values" {
+  type        = any
+  default     = {}
+  description = "AWS ECS configuration"
+}
+
+variable "lb_values" {
+  type        = any
+  default     = {}
+  description = "AWS Load Balancer configuration"
+}
+
+variable "vpc_values" {
+  type        = any
+  default     = {}
+  description = "AWS Load Balancer configuration"
+}
+
+variable "container" {
+  type        = any
+  default     = {}
+  description = "Container configuration to deploy"
+}
